193884173b7720bdf83c92c4358361109a451e1d
Derek <derek7mc@gmail.com>
2020-11-26 14:15:10 -0700

    client functions

---
+++ cookiejar-client.py
@@ -0,0 +1,15 @@
+
+# TODO standalone client commands
+# newroot
+# setroot
+# addsite (site-id) [short-name] [site-url]
+#  Note: all further commands require the client's rootcookie to be set.
+# sitecookie (siteid or short-name)
+# siteprecookie (siteid or short-name) -- only used the first time you connect to a new service
+# authhash (salt-_n) (siteid or short-name)  -- prompts to mark the authhash as used.
+# backupsecret -- (backup-version-id) (
+#  Save/Load:
+# encryptstore -- (key) encrypts and writes the current client state to a datastore 
+# loadstore -- (key) loads an encrypted datastore.
+
+
--- cookiejar.py
+++ cookiejar.py
@@ -227,19 +227,6 @@ def showSiteId(args):
 def setUserCookie(args):
     print("TODO")
 
-# TODO standalone client commands
-# newroot
-# setroot
-# addsite (site-id) [short-name] [site-url]
-#  Note: all further commands require the client's rootcookie to be set.
-# sitecookie (siteid or short-name)
-# siteprecookie (siteid or short-name) -- only used the first time you connect to a new service
-# authhash (salt-_n) (siteid or short-name)  -- prompts to mark the authhash as used.
-# backupsecret -- (backup-version-id) (
-#  Save/Load:
-# encryptstore -- (key) encrypts and writes the current client state to a datastore 
-# loadstore -- (key) loads an encrypted datastore.
-
 if __name__ == "__main__":
     commands = [
         ["id", showSiteId, "show the 'Site Id' which serves as a salt for generating the 'user cookie' and other data."],
